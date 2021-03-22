aa2a0ebe31cbc3adb2957d83f0d71b54b6761816
sydney <sydneychase5841@gmail.com>
2021-03-21 22:21:14 -0400

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const main = document.getElementById(`main`);
+main.remove()
+
+const newHeader = document.createElement(`h1`);
+newHeader.id = `victory`;
+
+newHeader.innerHTML = `Sydney is the champion`
+
+
+
+
+
 // Write your code here!
